50adfcfe973649631df1c7ad8bdb3f4f8f9e283e
Morgan Winbush <morgan.winbush@method.com>
2021-08-10 14:28:19 -0400

    Initial Commit

---
+++ Generic/GenericApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericApp.swift
+//  Generic
+//
+//  Created by Morgan Winbush on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct GenericApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
